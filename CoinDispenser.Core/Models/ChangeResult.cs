09bc28fd8ffb9465016737c25347d05736d5324e
Nhlelo <nhlelow@gmail.com>
2025-10-22 23:50:22 +0200

    Add Model for ChangeResult

---
+++ CoinDispenser.Core/Models/ChangeResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinDispenser.Core.Models
+{
+    public class ChangeResult
+    {
+        public bool Success { get; init; }
+        public int TotalCoins { get; init; }
+        public IReadOnlyDictionary<int, int> Coins { get; init; } = new Dictionary<int, int>();
+        public string? Error { get; init; }
+
+    }
+}
