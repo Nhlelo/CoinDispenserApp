652861f15cd99605f2dcff22ec70b595611fcb4d
Nhlelo <nhlelow@gmail.com>
2025-10-22 23:52:36 +0200

    Add code first approach for db creation plus migrations

---
+++ CoinDispenser.Api/Data/CoinChangeDbContext.cs
@@ -0,0 +1,13 @@
+﻿using CoinDispenser.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoinDispenser.Api.Data
+{
+    // EF Core DbContext that stores request history and results
+    public class CoinChangeDbContext : DbContext
+    {
+        public CoinChangeDbContext(DbContextOptions<CoinChangeDbContext> options) : base(options) { }
+        public DbSet<ChangeEntity> ChangeRequests { get; set; } = null!;
+    }
+
+}
