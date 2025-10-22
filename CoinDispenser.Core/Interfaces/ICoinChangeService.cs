807829cded18a122dc75d48816fe5f395a194c40
Nhlelo <nhlelow@gmail.com>
2025-10-22 23:51:04 +0200

    Add Interface for CoinChange Service

---
+++ CoinDispenser.Core/Interfaces/ICoinChangeService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CoinDispenser.Core.Models;
+
+namespace CoinDispenser.Core.Interfaces
+{
+    // Defines the contract for the coin change method
+    public interface ICoinChangeService
+    {
+        ChangeResult GetMinimumCoins(IReadOnlyList<int> denominations, int amount);
+    }
+
+}
