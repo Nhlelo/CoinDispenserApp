dc5520ebf19a1d0e49f85181602a7f403730838d
Nhlelo <nhlelow@gmail.com>
2025-10-23 19:35:02 +0200

    Add DTO

---
+++ CoinDispenser.Api/DTOs/ChangeRequestDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoinDispenser.Api.DTOs
+{
+    // Request DTO validated by model binding
+    public class ChangeRequestDto
+    {
+        [Required]
+        [MinLength(1)]
+        public List<int>? Denominations { get; set; }
+
+        [Range(0, int.MaxValue)]
+        public int Amount { get; set; }
+    }
+
+}
